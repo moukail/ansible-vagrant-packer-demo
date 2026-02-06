d52dc9bcf1461592b1827a2aa5be24d936b21f89
I Moukafih <i.moukafih@gmail.com>
2026-02-06 20:05:19 +0100

    update

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -eux
 
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.5.tar.xz
-tar -xf linux-6.18.5.tar.xz
-cd linux-6.18.5
+sudo dnf install -y bc flex bison elfutils-libelf-devel openssl-devel
 
+cd $(mktemp -d)
+
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.5.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.197.tar.xz
+
+tar -xf linux-5.15.197.tar.xz
+cd linux-5.15.197
+
+make clean
+make mrproper
+
+#cp /boot/config-$(uname -r) .config
 make defconfig
 make prepare
 make -j$(nproc)
